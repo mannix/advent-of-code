e574b50a45e0b77c85f5889d0db56fae93487414
Mike Mannix <mike.mannix@gmail.com>
2020-12-05 08:31:25 -0500

    2020 day 2 part 2

--- 2020/d02/d02.js
+++ 2020/d02/d02.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const inputData = () => {
   const raw = fs.readFileSync('input', 'utf8');
   const rawList = raw.split(/\r?\n/);
+  rawList.pop();
   return rawList.map(x => x.split(": "));
 }
 
-const validPasswords = (passwords) => {
-  passwords.pop();
+const partOne = (passwords) => {
   return passwords.filter(password => {
     const rule = password[0];
     const phrase = password[1];
@@ -18,5 +18,21 @@ const validPasswords = (passwords) => {
     return occurances >= min && occurances <= max;
   }).length;
 }
+
+const partTwo = (passwords) => {
+  return passwords.filter(password => {
+    const rule = password[0];
+    const phrase = password[1];
+    const firstSpot = rule.substring(0, rule.indexOf("-"));
+    const secondSpot = rule.substring(rule.indexOf("-") + 1, rule.indexOf(" "));
+    const req = rule.substr(rule.indexOf(" ") + 1);
+    let matches = 0;
+    if (phrase[firstSpot - 1] == req) { matches++; }
+    if (phrase[secondSpot - 1] == req) { matches++; }
+    return matches == 1;
+  }).length;
+}
+
 const allPasswords = inputData();
-console.log(`Number of valid passwords: ${validPasswords(allPasswords)}`);
+console.log(`Part One: ${partOne(allPasswords)}`);
+console.log(`Part Two: ${partTwo(allPasswords)}`);
