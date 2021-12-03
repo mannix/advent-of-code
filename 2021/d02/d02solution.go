9bca49c5c3c738e7e79cda3635d032e945531bb7
Mike Mannix <mike.mannix@gmail.com>
2021-12-03 09:04:08 -0500

    Day 2 part 2

--- 2021/d02/d02solution.go
+++ 2021/d02/d02solution.go
@@ -12,6 +12,7 @@ func main() {
 		instructions := strings.Split(string(input), "\n")
 		hor := 0
 		dep := 0
+		aim := 0
 		for i, instruction := range instructions {
 			if i == len(instructions)-1 {
 				break
@@ -20,13 +21,12 @@ func main() {
 			if amt, err := strconv.Atoi(parts[1]); err == nil {
 				switch parts[0] {
 				case "up":
-					dep -= amt
+					aim -= amt
 				case "down":
-					dep += amt
+					aim += amt
 				case "forward":
 					hor += amt
-				default:
-					println(parts[0])
+					dep += aim * amt
 				}
 			}
 		}
