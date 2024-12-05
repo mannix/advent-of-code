ac77367241ff8842d10f7a08ee8ca5456704f705
Mike Mannix <mike.mannix@contractors.mailchimp.com>
2024-12-05 15:49:01 -0500

    2024 day 3 part 1

--- 2024/day3.rb
+++ 2024/day3.rb
@@ -1,15 +1,14 @@
-test_input = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
+test_input = ['xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))']
 
 case ARGV[0]
 when 'test'
   input = test_input
 when 'solve'
-  input = File.read('input/day3_input.txt').split("\n")
+  input = File.read('input/day3_input.txt')
 else
   abort 'test or solve?'
 end
 
-# maybe split string based on patter?
-pattern = /mul\(\d{1,3},\d{1,3}\)/
-puts pattern.match?('weofij')
-puts pattern.match?('mul(1,2)')
+match = input.scan(/mul\((\d{1,3}),(\d{1,3})\)/)
+total = match.sum { |a, b| a.to_i * b.to_i }
+puts "Total: #{total}"
